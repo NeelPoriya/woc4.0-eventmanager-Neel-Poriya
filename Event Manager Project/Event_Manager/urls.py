fd54793ea8f8750259949c82338347b7da8c58d6
Neel Poriya <neelporiya1309@gmail.com>
2022-06-21 20:49:00 +0530

    Some changes 3

--- Event Manager Project/Event_Manager/urls.py
+++ Event Manager Project/Event_Manager/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from registration.views import events
 
 from django.views.static import serve
 from django.conf import *
@@ -22,6 +23,7 @@ from django.conf import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('registration.urls')),
+    path('/', events, name='event'),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
