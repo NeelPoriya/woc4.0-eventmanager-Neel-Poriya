47706f3818ec7164a26d2fd692ce398e0a2f44a9
Neel Poriya <neelporiya1309@gmail.com>
2022-06-21 20:53:39 +0530

    Some changes 3

--- Event Manager Project/Event_Manager/settings.py
+++ Event Manager Project/Event_Manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ls#vs$0d%82+u^lh0jzq3#2=6z98irv^z)z%)!tjtr9ch66!pz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1/events', 'event-manager-1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1/', 'event-manager-1.herokuapp.com']
 
 
 # Application definition
