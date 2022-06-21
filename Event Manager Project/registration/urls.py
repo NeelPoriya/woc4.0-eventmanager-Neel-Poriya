8047cbc23be6ad56fd947963a6d5bbc7323072cc
Neel Poriya <64574792+NeelPoriya@users.noreply.github.com>
2022-06-21 20:21:26 +0530

    Delete Event Manager Project directory

--- Event Manager Project/registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.events, name='event'),
-    path('register/', views.events_register, name='event_register'),
-    path('show_events/', views.show_events, name='participant-registration'),
-    path('dashboard/', views.event_dashboard, name='event-dashboard')
-]
--- Event Manager Project/static/js/input.js
+++
@@ -1,6 +0,0 @@
-function fun(x) {
-  document.getElementById(x).type = "date";
-}
-function fun2(x) {
-  document.getElementById(x).type = "time";
-}
